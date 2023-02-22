43e55a3ad51fb84e67fb08f1a2dfa5e2b680c258
Kemane AMH <kdonfack@amhconsulting-group.com>
2023-02-22 16:44:33 +0100

    first test

--- angular-14-crud-example/src/app/services/tutorial.service.ts
+++ angular-14-crud-example/src/app/services/tutorial.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Tutorial } from '../models/tutorial.model';
 
-const baseUrl = 'http://localhost:9090/api/tutorials';
+const baseUrl = 'http://sping:9090/api/tutorials';
 
 @Injectable({
   providedIn: 'root'
--- spring-boot-h2-database-crud/src/main/java/com/bezkoder/spring/jpa/h2/controller/TutorialController.java
+++ spring-boot-h2-database-crud/src/main/java/com/bezkoder/spring/jpa/h2/controller/TutorialController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.bezkoder.spring.jpa.h2.model.Tutorial;
 import com.bezkoder.spring.jpa.h2.repository.TutorialRepository;
 
-@CrossOrigin(origins = "http://localhost:8081")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api")
 public class TutorialController {
