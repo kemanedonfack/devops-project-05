938ac9f23b5900fa4599d85a72ecb3ae6f3f94e0
Kemane AMH <kdonfack@amhconsulting-group.com>
2023-02-24 15:07:13 +0100

    update cors

--- spring-boot-h2-database-crud/src/main/java/com/bezkoder/spring/jpa/h2/controller/TutorialController.java
+++ spring-boot-h2-database-crud/src/main/java/com/bezkoder/spring/jpa/h2/controller/TutorialController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.bezkoder.spring.jpa.h2.model.Tutorial;
 import com.bezkoder.spring.jpa.h2.repository.TutorialRepository;
 
-@CrossOrigin(origins = {"http://localhost:9090", "http://spring-api:9090"})
+@CrossOrigin(origins = {"http://localhost:5050", "http://spring-api:5050"})
 @RestController
 @RequestMapping("/api")
 public class TutorialController {
